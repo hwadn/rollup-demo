333dd0c92038205a6d9072893e539be26ca0c925
IamHuadong <1761339618@qq.com>
2022-07-31 15:32:58 +0800

    feat: update

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,29 @@
-import typescript from 'rollup-plugin-typescript2';
-import ttypescript from 'ttypescript'
+// import typescript from 'rollup-plugin-typescript2';
+// import ttypescript from 'ttypescript'
+// import { babel } from '@rollup/plugin-babel';
+import alias from '@rollup/plugin-alias';
+import path from 'path'
+import resolve from '@rollup/plugin-node-resolve'
+import commonjs from '@rollup/plugin-commonjs';
+import { exec } from "child_process";
+import typescript from '@rollup/plugin-typescript';
 
+const tscAlias = () => {
+    return {
+        name: "tsAlias",
+        writeBundle: () => {
+            return new Promise((resolve, reject) => {
+                exec("tsc-alias", function callback(error, stdout, stderr) {
+                    if (stderr || error) {
+                        reject(stderr || error);
+                    } else {
+                        resolve(stdout);
+                    }
+                });
+            });
+        },
+    };
+};
 export default [
 	{
 		input: 'src/index.ts',
@@ -15,18 +38,15 @@ export default [
 			}
 		],
 		plugins: [
-			typescript({
-				typescript: ttypescript,
-				tsconfig: './tsconfig.json',
-				tsconfigOverride: {
-					compilerOptions: {
-					  plugins: [
-						{ "transform": "typescript-transform-paths" },
-						{ "transform": "typescript-transform-paths", "afterDeclarations": true }
-					  ]
-					}
-				}
-			})
+			alias({
+				entries: [
+				  { find: '@', replacement: path.resolve(__dirname, 'src') },
+				]
+			}),
+			resolve({ extensions: ['.svg'] }),
+			commonjs(),
+			typescript({ tsconfig: './tsconfig.json', sourceMap: false }),
+			tscAlias(),
 		]
 	}
   ];
